9f5e300b9d0ac3f2db1d8c8e11f511150468e2ae
magvan temuulen <magvan.temuulen@gmail.com>
2023-01-13 17:27:36 +0100

    navbar

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, numericality: { only_integer: true}
+  validates :price, comparison: { greater_than: 0}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts
+  has_many :products
+  has_many :bought_products, through: :posts, source: :product, dependent: :destroy
+
+  validates :name, :last_name, :address, :age, :email, :password, presence: true
+  validates :email, uniqueness: true
 end
